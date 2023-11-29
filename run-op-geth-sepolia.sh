c6f7a050362edc3cb817193f3c8a3275beda0022
Danny Willems <be.danny.willems@gmail.com>
2023-11-29 15:24:49 +0100

    Update op-geth-sepolia with var

--- run-op-geth-sepolia.sh
+++ run-op-geth-sepolia.sh
@@ -1,30 +1,39 @@
 SEQUENCER_URL="https://sepolia-sequencer.optimism.io/"
 
-DATA_DIRECTORY=$(pwd)/data-op-geth-sepolia
+DATADIR=$(pwd)/data-op-geth-sepolia
 JWT_TOKEN=$(pwd)/jwt.txt
+
+LISTENING_ADDR="0.0.0.0"
+AUTHRPC_PORT=8551
+HTTP_PORT=8545
+WS_PORT=8546
+LISTENING_PORT=30303
+DISCOVERY_PORT=30303
 
 cd ../../op-geth
 
 ./build/bin/geth \
-	--datadir=${DATA_DIRECTORY} \
-        --http \
-	--http.port=8545 \
-        --http.corsdomain="*" \
-        --http.vhosts="*" \
-        --http.addr=localhost \
-        --http.api=web3,debug,eth,txpool,net,engine \
-        --ws \
-        --ws.addr=localhost \
-        --ws.port=8546 \
-        --ws.origins="*" \
-        --ws.api=debug,eth,txpool,net,engine \
-        --syncmode=full \
-        --gcmode=archive \
-        --nodiscover \
-        --maxpeers=0 \
-        --authrpc.vhosts="*" \
-        --authrpc.addr=localhost \
-        --authrpc.port=8551 \
-        --authrpc.jwtsecret=${JWT_TOKEN} \
-	--rollup.sequencerhttp=$SEQUENCER_URL \
-        --rollup.disabletxpoolgossip=true
+    --datadir=${DATADIR} \
+    --http \
+    --http.port=${HTTP_PORT} \
+    --http.corsdomain="*" \
+    --http.vhosts="*" \
+    --http.addr=${LISTENING_ADDR} \
+    --http.api=web3,debug,eth,txpool,net,engine \
+    --syncmode=full \
+    --gcmode=archive \
+    --nodiscover \
+    --maxpeers=0 \
+    --ws \
+    --ws.addr=${LISTENING_ADDR} \
+    --ws.port=${WS_PORT} \
+    --ws.origins="*" \
+    --ws.api=debug,eth,txpool,net,engine \
+    --authrpc.vhosts="*" \
+    --authrpc.addr=${LISTENING_ADDR} \
+    --authrpc.port=${AUTHRPC_PORT} \
+    --authrpc.jwtsecret=${JWT_TOKEN} \
+    --port=${LISTENING_PORT} \
+    --discovery.port=${DISCOVERY_PORT} \
+    --rollup.sequencerhttp=$SEQUENCER_URL \
+    --rollup.disabletxpoolgossip=true
